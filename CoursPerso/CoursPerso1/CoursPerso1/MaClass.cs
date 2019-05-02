75a80dee8f92cef3ba1e31148cecd9090d5d52e2
malcom007 <malcom.m@live.ca>
2019-05-02 10:10:39 -0400

    test

---
+++ CoursPerso/CoursPerso1/CoursPerso1/MaClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CoursPerso1
+{
+    public class MaClass
+    {
+       
+            private String _Nom, _Prenom;
+            private int _Age;
+
+            public MaClass(String nom, String prenom, int age)
+        {
+            this._Nom = nom;
+            this._Prenom = prenom;
+            this._Age = age;
+        }
+
+        public String GetIdentity()
+        {
+            return "Je m'appelle "+_Nom+" "+_Prenom+" j'ai "+_Age;
+        }
+
+        public void SetAge(int age)
+        {
+            this._Age = age;
+
+        }
+
+        }
+
+}
+

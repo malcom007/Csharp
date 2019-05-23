f6ee549cdb6a16a4a0b96477e02f41d1c3f5eeef
malcom007 <malcom.m@live.ca>
2019-05-23 10:16:52 -0400

    Create tailleTabEtudiantAtteint.cs

---
+++ ExamenIntraGestionEtudiants/ExamenIntraGestionEtudiants/tailleTabEtudiantAtteint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ExamenIntraGestionEtudiants
+{
+    [Serializable]
+    internal class tailleTabEtudiantAtteint : Exception
+    {
+        public tailleTabEtudiantAtteint()
+        {
+        }
+
+        public tailleTabEtudiantAtteint(string message) : base(message)
+        {
+        }
+
+        public tailleTabEtudiantAtteint(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected tailleTabEtudiantAtteint(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

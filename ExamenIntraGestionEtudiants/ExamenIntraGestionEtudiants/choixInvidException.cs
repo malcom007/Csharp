a2e7c9c75bb1f170adde7a76ef534ad9da509ec8
malcom007 <malcom.m@live.ca>
2019-05-23 09:25:34 -0400

    Creating Methode

---
+++ ExamenIntraGestionEtudiants/ExamenIntraGestionEtudiants/choixInvidException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ExamenIntraGestionEtudiants
+{
+    [Serializable]
+    internal class choixInvidException : Exception
+    {
+        public choixInvidException()
+        {
+        }
+
+        public choixInvidException(string message) : base(message)
+        {
+        }
+
+        public choixInvidException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected choixInvidException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

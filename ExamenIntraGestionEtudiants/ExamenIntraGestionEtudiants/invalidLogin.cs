149f151dd2e00ac80f57ffc509e5e8cf95ad86c1
malcom007 <malcom.m@live.ca>
2019-05-23 08:53:45 -0400

    Create invalidLogin.cs

---
+++ ExamenIntraGestionEtudiants/ExamenIntraGestionEtudiants/invalidLogin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ExamenIntraGestionEtudiants
+{
+    [Serializable]
+    internal class invalidLogin : Exception
+    {
+        public invalidLogin()
+        {
+        }
+
+        public invalidLogin(string message) : base(message)
+        {
+        }
+
+        public invalidLogin(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected invalidLogin(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

8211a0c9df06b33289b0765f87e5d3525ad01a08
malcom007 <malcom.m@live.ca>
2019-05-23 15:37:22 -0400

    Create nombreEtudiantAtteint.cs

---
+++ ExamenIntraGestionEtudiants/ExamenIntraGestionEtudiants/nombreEtudiantAtteint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ExamenIntraGestionEtudiants
+{
+    [Serializable]
+    internal class nombreEtudiantAtteint : Exception
+    {
+        public nombreEtudiantAtteint()
+        {
+        }
+
+        public nombreEtudiantAtteint(string message) : base(message)
+        {
+        }
+
+        public nombreEtudiantAtteint(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected nombreEtudiantAtteint(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

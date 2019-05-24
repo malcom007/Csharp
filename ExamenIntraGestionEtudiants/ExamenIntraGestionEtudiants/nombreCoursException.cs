e8368d4df0b9293a760d8476dcffae7cd9043441
malcom007 <malcom.m@live.ca>
2019-05-24 08:31:57 -0400

    Create nombreCoursException.cs

---
+++ ExamenIntraGestionEtudiants/ExamenIntraGestionEtudiants/nombreCoursException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ExamenIntraGestionEtudiants
+{
+    [Serializable]
+    internal class nombreCoursException : Exception
+    {
+        public nombreCoursException()
+        {
+        }
+
+        public nombreCoursException(string message) : base(message)
+        {
+        }
+
+        public nombreCoursException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected nombreCoursException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

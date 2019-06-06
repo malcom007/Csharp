8c7419a36ce3b146ba93a125f3b205a6a5d5d497
malcom007 <malcom.m@live.ca>
2019-06-06 08:42:17 -0400

    Create Etudiant.cs

---
+++ POO/POO/Etudiant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace POO
+{
+    public class Etudiant
+    {
+        private int numEtudiant;
+        private string nom;
+
+        public int m_umEtudiant
+        {
+            get
+            {
+                return numEtudiant;
+            }
+
+            set
+            {
+                numEtudiant = value;
+            }
+        }
+
+        public string m_nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        //constructeur vide
+        public Etudiant()
+        {
+        }
+
+        //constructeur avec valeurs
+        public Etudiant(int numEtudiant, string nom)
+        {
+            this.numEtudiant = numEtudiant;
+            this.nom = nom;
+        }
+
+    }
+}

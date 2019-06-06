4685a73f3d18c4e2449702d0f74050d876753529
malcom007 <malcom.m@live.ca>
2019-06-06 08:24:32 -0400

    POO

---
+++ POO/Projet2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projet2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
